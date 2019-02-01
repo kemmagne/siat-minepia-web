c1489e09f4773faf51c007e1b91952d9178591b6
Marius YENKE MBEUYO <yenkemarius@yahoo.fr>
2019-02-01 17:27:53 +0100

    Mise à jour du rapport du VTD MINSANTE

--- src/main/java/org/guce/siat/web/reports/exporter/VtdMinsanteExporter.java
+++ src/main/java/org/guce/siat/web/reports/exporter/VtdMinsanteExporter.java
@@ -1,5 +1,6 @@
 package org.guce.siat.web.reports.exporter;
 
+import java.io.ByteArrayInputStream;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Collections;
@@ -13,6 +14,10 @@ import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang.StringUtils;
 import org.guce.siat.common.model.File;
 import org.guce.siat.common.model.FileFieldValue;
+import org.guce.siat.common.model.FileItem;
+import org.guce.siat.common.model.FileItemFieldValue;
+import org.guce.siat.common.utils.QRCodeUtils;
+import org.guce.siat.web.reports.vo.VtdMinsanteFileItemVo;
 import org.guce.siat.web.reports.vo.VtdMinsanteFileVo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -55,11 +60,32 @@ public class VtdMinsanteExporter extends AbstractReportInvoker {
 		final VtdMinsanteFileVo vtdMinsanteVo = new VtdMinsanteFileVo();
 		String bl = null;
 		String lta = null;
+		String supplierPhoneCode = null;
+		String supplierPhoneNumber = null;
+		String supplierMobileCode = null;
+		String supplierMobileNumber = null;
+		String supplierFaxCode = null;
+		String supplierFaxNumber = null;
 		if ((file != null)) {
 			vtdMinsanteVo.setDecisionPlace(file.getBureau().getAddress());
 			vtdMinsanteVo.setDecisionDate(file.getSignatureDate());
 			final List<FileFieldValue> fileFieldValueList = file.getFileFieldValueList();
 			java.util.Date signatoryDate = null;
+			
+			if (file.getClient() != null){
+				vtdMinsanteVo.setClientAddress(file.getClient().getFirstAddress());
+				vtdMinsanteVo.setClientCity(file.getClient().getCity());
+				if (file.getClient().getCountry() != null)
+					vtdMinsanteVo.setClientCountry(file.getClient().getCountry().getCountryName());
+				vtdMinsanteVo.setClientInscriptionCode(file.getClient().getCommerceApprovalRegistrationNumberFile());
+				vtdMinsanteVo.setClientInscriptionDate(file.getClient().getCommerceApprovalObtainedDate());
+				vtdMinsanteVo.setClientInscriptionIssueDate(file.getClient().getCommerceApprovalValidityDate());
+				vtdMinsanteVo.setClientMail(file.getClient().getEmail());
+				vtdMinsanteVo.setClientName(file.getClient().getCompanyName());
+				vtdMinsanteVo.setClientPhone(file.getClient().getPhone() + "/" + file.getClient().getMobile());
+				vtdMinsanteVo.setClientPobox(file.getClient().getPostalCode());
+				vtdMinsanteVo.setClientTaxpayerNumber(file.getClient().getNumContribuable());
+			}		
 
 			if (CollectionUtils.isNotEmpty(fileFieldValueList)) {
 				for (final FileFieldValue fileFieldValue : fileFieldValueList) {
@@ -95,6 +121,12 @@ public class VtdMinsanteExporter extends AbstractReportInvoker {
 							vtdMinsanteVo.setLoadingCustomsOffice(fileFieldValue.getValue());
 							break;
 						case "INFORMATIONS_GENERALES_TRANSPORT_MODE_TRANSPORT_LIBELLE":
+							vtdMinsanteVo.setTransportMode(fileFieldValue.getValue());
+							break;
+						case "INFORMATIONS_GENERALES_TERMES_VENTE_CODE_DEVISE":
+							vtdMinsanteVo.setCurrency(fileFieldValue.getValue());
+							break;
+						case "INFORMATIONS_GENERALES_TRANSPORT_MOYEN_TRANSPORT_LIBELLE":
 							vtdMinsanteVo.setTransportWay(fileFieldValue.getValue());
 							break;
 						case "INFORMATIONS_GENERALES_LIEU_DECHARGEMENT_LIBELLE":
@@ -103,6 +135,39 @@ public class VtdMinsanteExporter extends AbstractReportInvoker {
 						case "FOURNISSEUR_RAISONSOCIALE":
 							vtdMinsanteVo.setSupplierName(fileFieldValue.getValue());
 							break;
+						case "FOURNISSEUR_ADRESSE_ADRESSE1":
+							vtdMinsanteVo.setSupplierAddress(fileFieldValue.getValue());
+							break;
+						case "FOURNISSEUR_ADRESSE_PAYSADDRESS_NOMPAYS":
+							vtdMinsanteVo.setSupplierCountry(fileFieldValue.getValue());
+							break;
+						case "FOURNISSEUR_ADRESSE_VILLE":
+							vtdMinsanteVo.setSupplierCity(fileFieldValue.getValue());
+							break;
+						case "FOURNISSEUR_ADRESSE_BP":
+							vtdMinsanteVo.setSupplierPobox(fileFieldValue.getValue());
+							break;
+						case "FOURNISSEUR_ADRESSE_ADRESSEELECTRONIQUE":
+							vtdMinsanteVo.setSupplierMail(fileFieldValue.getValue());
+							break;
+						case "FOURNISSEUR_TELEPHONE_FIXE_INDICATIFPAYS":
+							supplierPhoneCode = fileFieldValue.getValue();
+							break;
+						case "FOURNISSEUR_TELEPHONE_FIXE_NUMERO":
+							supplierPhoneNumber = fileFieldValue.getValue();
+							break;
+						case "FOURNISSEUR_TELEPHONE_MOBILE_INDICATIFPAYS":
+							supplierMobileCode = fileFieldValue.getValue();
+							break;
+						case "FOURNISSEUR_TELEPHONE_MOBILE_NUMERO":
+							supplierMobileNumber = fileFieldValue.getValue();
+							break;
+						case "FOURNISSEUR_FAX_INDICATIFPAYS":
+							supplierFaxCode = fileFieldValue.getValue();
+							break;
+						case "FOURNISSEUR_FAX_NUMERO":
+							supplierFaxNumber = fileFieldValue.getValue();
+							break;
 						case "INFORMATIONS_GENERALES_TRANSPORT_NUM_CONNAISSEMENT_LTA":
 							lta = fileFieldValue.getValue();
 							break;
@@ -110,13 +175,14 @@ public class VtdMinsanteExporter extends AbstractReportInvoker {
 							bl = fileFieldValue.getValue();
 							break;
 						case "SIGNATAIRE_DATE":
-							if (StringUtils.isNotBlank(fileFieldValue.getValue())) {
-								try {
-									signatoryDate = new SimpleDateFormat("dd/MM/yyyy").parse(fileFieldValue.getValue());
-								} catch (final ParseException e) {
-									LOG.error(Objects.toString(e), e);
-								}
-							}
+							vtdMinsanteVo.setSignatoryDate(fileFieldValue.getValue());
+//							if (StringUtils.isNotBlank(fileFieldValue.getValue())) {
+//								try {
+//									signatoryDate = new SimpleDateFormat("dd/MM/yyyy").parse(fileFieldValue.getValue());
+//								} catch (final ParseException e) {
+//									LOG.error(Objects.toString(e), e);
+//								}
+//							}
 							break;
 						case "DATE_VALIDITE":
 							vtdMinsanteVo.setExpirationDate(fileFieldValue.getValue());
@@ -126,13 +192,56 @@ public class VtdMinsanteExporter extends AbstractReportInvoker {
 							break;
 					}
 				}
+				final List<FileItem> fileItemsList = file.getFileItemsList();
+			if (CollectionUtils.isNotEmpty(fileItemsList)){
+				for (FileItem fileItem : fileItemsList){
+					final VtdMinsanteFileItemVo fileItemVo = new VtdMinsanteFileItemVo();
+					
+					fileItemVo.setQuantity(Double.parseDouble(fileItem.getQuantity()));
+					fileItemVo.setFobValue(fileItem.getFobValue());
+					for (FileItemFieldValue fileItemFieldValue : fileItem.getFileItemFieldValueList()){
+						switch (fileItemFieldValue.getFileItemField().getCode()){
+							case "AMM":
+								fileItemVo.setAmm(fileItemFieldValue.getValue());
+								break;
+							case "UNITE":
+								fileItemVo.setUnit(fileItemFieldValue.getValue());
+								break;
+							case "NOM_COMMERCIAL":
+								fileItemVo.setDesignation(fileItemFieldValue.getValue());
+								break;
+							default:
+								break;
+						}
+					}
+					if (fileItem.getNsh() != null)
+						fileItemVo.setCode(fileItem.getNsh().getGoodsItemCode());
+					if (fileItemVo.getDesignation() == null || StringUtils.isEmpty(fileItemVo.getDesignation())){
+						if (fileItem.getNsh() != null){
+							fileItemVo.setDesignation(fileItem.getNsh().getGoodsItemDesc());
+						}
+					}
+					vtdMinsanteVo.getFileItemList().add(fileItemVo);
+				}
 			}
+			}
+			vtdMinsanteVo.setAttestation(file.getNumeroDemande());
+			vtdMinsanteVo.setSupplierPhone(supplierPhoneCode + supplierPhoneNumber + "/" + supplierMobileCode + supplierMobileNumber);
+			vtdMinsanteVo.setSupplierFax(supplierFaxCode + supplierFaxNumber);
 			vtdMinsanteVo.setBillOfLanding(lta == null ? bl : (lta + (bl == null ? "" : " / " + bl)));
-			vtdMinsanteVo.setAttestation(vtdMinsanteVo.getDecisionNumber()
-					+ PREFIX
-					+ new SimpleDateFormat("dd/MM/yyyy").format(signatoryDate == null ? java.util.Calendar.getInstance().getTime()
-							: signatoryDate));
+//			vtdMinsanteVo.setAttestation(vtdMinsanteVo.getDecisionNumber()
+//					+ PREFIX
+//					+ new SimpleDateFormat("dd/MM/yyyy").format(signatoryDate == null ? java.util.Calendar.getInstance().getTime()
+//							: signatoryDate));
 		}
+		
+		
+		String qrContent = "N° Dossier : " + file.getNumeroDemande() +
+				" N° Facture : " + vtdMinsanteVo.getInvoiceNumber() +
+				" Date facture : " + vtdMinsanteVo.getInvoiceDate() + 
+				" Importateur : " + vtdMinsanteVo.getClientName() + 
+				" Fournisseur : " + vtdMinsanteVo.getSupplierName();
+		vtdMinsanteVo.setQrCode(new ByteArrayInputStream(QRCodeUtils.generateQR(qrContent, 512)));
 
 		return new JRBeanCollectionDataSource(Collections.singleton(vtdMinsanteVo));
 	}
--- src/main/java/org/guce/siat/web/reports/vo/VtdMinsanteFileItemVo.java
+++ src/main/java/org/guce/siat/web/reports/vo/VtdMinsanteFileItemVo.java
@@ -9,6 +9,10 @@ public class VtdMinsanteFileItemVo extends AbstractFileItemVo
 {
 	/** The code. */
 	private String code;
+	
+	private String amm;
+	private String unit;
+	private String fobValue;
 
 	/**
 	 * Gets the code.
@@ -30,5 +34,31 @@ public class VtdMinsanteFileItemVo extends AbstractFileItemVo
 	{
 		this.code = code;
 	}
+
+	public String getAmm() {
+		return amm;
+	}
+
+	public void setAmm(String amm) {
+		this.amm = amm;
+	}
+
+	public String getUnit() {
+		return unit;
+	}
+
+	public void setUnit(String unit) {
+		this.unit = unit;
+	}
+
+	public String getFobValue() {
+		return fobValue;
+	}
+
+	public void setFobValue(String fobValue) {
+		this.fobValue = fobValue;
+	}
+	
+	
 
 }
