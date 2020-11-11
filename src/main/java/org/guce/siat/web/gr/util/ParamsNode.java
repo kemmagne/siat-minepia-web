c8ab36285f66a12edf9e8dfec67ce7717148295f
TADZOTSA Hyacinthe <romuald.tadzotsa@guichetunique.org>
2020-11-11 15:40:50 +0100

    allow the edition of parameter relative to ebxml backup and change the way label of param is obtained

--- src/main/java/org/guce/siat/web/gr/util/ParamsNode.java
+++ src/main/java/org/guce/siat/web/gr/util/ParamsNode.java
@@ -1,106 +1,93 @@
 package org.guce.siat.web.gr.util;
 
-
-
 /**
  * The Class ParamsNode.
  */
-public class ParamsNode
-{
+public class ParamsNode {
 
-	/** The name. */
-	private String name;
+    /**
+     * The name.
+     */
+    private String name;
 
-	/** The value. */
-	private String value;
+    /**
+     * The value.
+     */
+    private String value;
 
-	/** The Type. */
-	private String type;
+    /**
+     * The Type.
+     */
+    private String type;
 
+    /**
+     * Instantiates a new params node.
+     */
+    public ParamsNode() {
+    }
 
+    /**
+     * Instantiates a new params node.
+     *
+     * @param name the name
+     * @param value the value
+     * @param type the type
+     */
+    public ParamsNode(final String name, final String value, final String type) {
+        super();
+        this.name = name;
+        this.value = value;
+        this.type = type;
+    }
 
-	/**
-	 * Instantiates a new params node.
-	 */
-	public ParamsNode()
-	{
-	}
+    /**
+     * Gets the name.
+     *
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
 
-	/**
-	 * Instantiates a new params node.
-	 *
-	 * @param name
-	 *           the name
-	 * @param value
-	 *           the value
-	 * @param type
-	 *           the type
-	 */
-	public ParamsNode(final String name, final String value, final String type)
-	{
-		super();
-		this.name = name;
-		this.value = value;
-		this.type = type;
-	}
+    /**
+     * Sets the name.
+     *
+     * @param name the name to set
+     */
+    public void setName(final String name) {
+        this.name = name;
+    }
 
-	/**
-	 * Gets the name.
-	 *
-	 * @return the name
-	 */
-	public String getName()
-	{
-		return name;
-	}
+    /**
+     * Gets the value.
+     *
+     * @return the value
+     */
+    public String getValue() {
+        return value;
+    }
 
-	/**
-	 * Sets the name.
-	 *
-	 * @param name
-	 *           the name to set
-	 */
-	public void setName(final String name)
-	{
-		this.name = name;
-	}
+    /**
+     * Sets the value.
+     *
+     * @param value the value to set
+     */
+    public void setValue(final String value) {
+        this.value = value;
+    }
 
-	/**
-	 * Gets the value.
-	 *
-	 * @return the value
-	 */
-	public String getValue()
-	{
-		return value;
-	}
+    /**
+     * @return the type
+     */
+    public String getType() {
+        return type;
+    }
 
-	/**
-	 * Sets the value.
-	 *
-	 * @param value
-	 *           the value to set
-	 */
-	public void setValue(final String value)
-	{
-		this.value = value;
-	}
-
-	/**
-	 * @return the type
-	 */
-	public String getType()
-	{
-		return type;
-	}
-
-	/**
-	 * @param type
-	 *           the type to set
-	 */
-	public void setType(final String type)
-	{
-		this.type = type;
-	}
+    /**
+     * @param type the type to set
+     */
+    public void setType(final String type) {
+        this.type = type;
+    }
 
 }
--- src/main/java/org/guce/siat/web/gr/util/enums/ParamsNames.java
+++ src/main/java/org/guce/siat/web/gr/util/enums/ParamsNames.java
@@ -1,76 +1,88 @@
 package org.guce.siat.web.gr.util.enums;
 
-
 /**
  * The Enum ParamsNames.
  */
-public enum ParamsNames
-{
+public enum ParamsNames {
 
-	/** The product known period. */
-	PRODUCT_KNOWN_PERIOD("ProductKnownPeriod"),
+    /**
+     * The product known period.
+     */
+    PRODUCT_KNOWN_PERIOD("ProductKnownPeriod"),
+    /**
+     * The product known threshold.
+     */
+    PRODUCT_KNOWN_THRESHOLD("ProductKnownThreshold"),
+    /**
+     * The negative decisions period.
+     */
+    NEGATIVE_DECISIONS_PERIOD("NegativeDecisionsPeriod"),
+    /**
+     * The product tested period.
+     */
+    PRODUCT_TESTED_PERIOD("ProductTestedPeriod"),
+    /**
+     * The importer known period.
+     */
+    IMPORTER_KNOWN_PERIOD("ImporterKnownPeriod"),
+    /**
+     * The importer known threshold.
+     */
+    IMPORTER_KNOWN_THRESHOLD("ImporterKnownThreshold"),
+    /**
+     * The quantity coefficient.
+     */
+    QUANTITY_COEFFICIENT("QuantityCoefficient"),
+    /**
+     * The rdd delay.
+     */
+    RDD_DELAY("RddDelay"),
+    /**
+     * The mecdelay.
+     */
+    MEC_DELAY("MecDelay"),
+    /**
+     * The nb rdd.
+     */
+    NB_RDD("NbRDD"),
+    /**
+     * The clearance delay.
+     */
+    CLEARANCE_DELAY("ClearanceDelay"),
+    /**
+     * The max attempts user connexion.
+     */
+    MAX_ATTEMPTS_USER_CONNEXION("MaxAttemptsUserConnexion"),
+    /**
+     * The token authentification.
+     */
+    TOKEN_AUTHENTIFICATION("TokenAuthentification"),
+    /**
+     * The max cancel request.
+     */
+    MAX_CANCEL_REQUEST("MaxCancelRequest");
 
-	/** The product known threshold. */
-	PRODUCT_KNOWN_THRESHOLD("ProductKnownThreshold"),
+    /**
+     * The code.
+     */
+    private final String code;
 
-	/** The negative decisions period. */
-	NEGATIVE_DECISIONS_PERIOD("NegativeDecisionsPeriod"),
+    /**
+     * Instantiates a new parametre.
+     *
+     * @param code the code
+     */
+    private ParamsNames(final String code) {
+        this.code = code.intern();
+    }
 
-	/** The product tested period. */
-	PRODUCT_TESTED_PERIOD("ProductTestedPeriod"),
+    /**
+     * Gets the code.
+     *
+     * @return the code
+     */
+    public String getCode() {
+        return this.code;
+    }
 
-	/** The importer known period. */
-	IMPORTER_KNOWN_PERIOD("ImporterKnownPeriod"),
-
-	/** The importer known threshold. */
-	IMPORTER_KNOWN_THRESHOLD("ImporterKnownThreshold"),
-
-	/** The quantity coefficient. */
-	QUANTITY_COEFFICIENT("QuantityCoefficient"),
-
-	/** The rdd delay. */
-	RDD_DELAY("RddDelay"),
-
-	/** The mecdelay. */
-	MEC_DELAY("MecDelay"),
-
-	/** The nb rdd. */
-	NB_RDD("NbRDD"),
-
-	/** The clearance delay. */
-	CLEARANCE_DELAY("ClearanceDelay"),
-
-	/** The max attempts user connexion. */
-	MAX_ATTEMPTS_USER_CONNEXION("MaxAttemptsUserConnexion"),
-
-	/** The token authentification. */
-	TOKEN_AUTHENTIFICATION("TokenAuthentification"),
-
-	/** The max cancel request. */
-	MAX_CANCEL_REQUEST("MaxCancelRequest");
-
-	/** The code. */
-	private final String code;
-
-
-	/**
-	 * Instantiates a new parametre.
-	 *
-	 * @param code
-	 *           the code
-	 */
-	private ParamsNames(final String code)
-	{
-		this.code = code.intern();
-	}
-
-	/**
-	 * Gets the code.
-	 *
-	 * @return the code
-	 */
-	public String getCode()
-	{
-		return this.code;
-	}
 }
